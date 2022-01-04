9f523fe3917a29c1c04c3b5f7be996973dbecfd6
Harry Yao <clearbamboo@outlook.com>
2022-01-04 12:12:57 +0800

    测试结果页基本完成

--- code/backend/bigjob/views.py
+++ code/backend/bigjob/views.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 from zhconv import convert
 import requests as req
-from .models import HollandQuestions
+from .models import HollandQuestions, BigJob
 from utils.http import HttpWrapper
 from utils.errors import MethodError, NoValue
 
@@ -13,16 +13,13 @@ from utils.errors import MethodError, NoValue
 @HttpWrapper
 def jobs(requests: HttpRequest):
     if requests.method == "GET":
-        a = {
-            "jobCat": "2005003008",
-            "bigJobImg": "https://static.104.com.tw/gtnew/img/character/bigRole/2005003008.png",
-            "jobName": "醫藥業務代表",
-            "jobDesc": "我會把藥賣出去，推廣客戶需要的醫療產品！",
-            "jobWorks": "拜訪固定或非固定地區醫院、診所、藥局客戶，以開發或維護客戶關係。\n銷售及推廣公司產品，例如醫療研究用儀器或醫藥耗材等。\n教育及示範產品使用方式。\n協助客戶使用產品。\n提供新客戶相關之行銷或經營輔導。\n宣導與執行公司的理念予店家。",
-            "subject1": "social",
-            "subject2": "biology"
-        }
-        result = {k: convert(v, 'zh-cn') for k, v in a.items()}
+        keys = (requests.GET.get('key1'),
+                requests.GET.get('key2'),
+                requests.GET.get('key3'))
+        options = {'holland1__in': keys,
+                   'holland2__in': keys,
+                   'holland3__in': keys}
+        result = list(BigJob.objects.filter(**options).values())
         return result
     else:
         raise MethodError
